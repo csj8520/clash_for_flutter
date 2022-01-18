91d0cdf123b0e13fc3fe6b08688c371655452daa
csj8520 <2372138557@qq.com>
2022-01-18 18:16:18 +0800

    update: connecting todo sort

--- lib/view/connections/index.dart
+++ lib/view/connections/index.dart
@@ -18,7 +18,7 @@ class TableItem {
   double width;
   Alignment align;
   bool tooltip;
-  String Function(ConnectionsConnection connect) getLabel;
+  String Function(ConnectionsConnection connect, ConnectionsConnection? preConnect) getLabel;
   TableItem({required this.head, required this.width, required this.align, this.tooltip = false, required this.getLabel});
 }
 
@@ -27,16 +27,30 @@ final List<TableItem> tableItems = [
       head: '域名',
       width: 260,
       align: Alignment.centerLeft,
-      getLabel: (c) => '${c.metadata.host.isNotEmpty ? c.metadata.host : c.metadata.sourceIP}:${c.metadata.destinationPort}'),
-  TableItem(head: '网络', width: 80, align: Alignment.center, getLabel: (c) => c.metadata.network),
-  TableItem(head: '类型', width: 120, align: Alignment.center, getLabel: (c) => c.metadata.type),
-  TableItem(head: '节点链', width: 200, align: Alignment.centerLeft, tooltip: true, getLabel: (c) => c.chains.reversed.join('/')),
-  TableItem(head: '规则', width: 140, align: Alignment.center, getLabel: (c) => '${c.rule}(${c.rulePayload})'),
-  TableItem(head: '速率', width: 100, align: Alignment.center, getLabel: (c) => '-'),
-  TableItem(head: '上传', width: 200, align: Alignment.center, getLabel: (c) => bytesToSize(c.upload)),
-  TableItem(head: '下载', width: 100, align: Alignment.center, getLabel: (c) => bytesToSize(c.download)),
-  TableItem(head: '来源IP', width: 140, align: Alignment.center, getLabel: (c) => c.metadata.sourceIP),
-  TableItem(head: '连接时间', width: 120, align: Alignment.center, getLabel: (c) => Day().useLocale(locale).from(Day.fromString(c.start))),
+      getLabel: (c, _) => '${c.metadata.host.isNotEmpty ? c.metadata.host : c.metadata.sourceIP}:${c.metadata.destinationPort}'),
+  TableItem(head: '网络', width: 80, align: Alignment.center, getLabel: (c, _) => c.metadata.network),
+  TableItem(head: '类型', width: 120, align: Alignment.center, getLabel: (c, _) => c.metadata.type),
+  TableItem(head: '节点链', width: 200, align: Alignment.centerLeft, tooltip: true, getLabel: (c, _) => c.chains.reversed.join('/')),
+  TableItem(head: '规则', width: 140, align: Alignment.center, getLabel: (c, _) => '${c.rule}(${c.rulePayload})'),
+  TableItem(
+    head: '速率',
+    width: 200,
+    align: Alignment.center,
+    getLabel: (c, p) {
+      // TODO: remove this
+      if (p == null) return '-';
+      final download = c.download - p.download;
+      final upload = c.upload - p.upload;
+      if (upload == 0 && download == 0) return '-';
+      if (upload == 0) return '↑ ${bytesToSize(upload)}/s';
+      if (download == 0) return '↓ ${bytesToSize(download)}/s';
+      return '↑ ${bytesToSize(upload)}/s ↓ ${bytesToSize(download)}/s';
+    },
+  ),
+  TableItem(head: '上传', width: 100, align: Alignment.center, getLabel: (c, _) => bytesToSize(c.upload)),
+  TableItem(head: '下载', width: 100, align: Alignment.center, getLabel: (c, _) => bytesToSize(c.download)),
+  TableItem(head: '来源IP', width: 140, align: Alignment.center, getLabel: (c, _) => c.metadata.sourceIP),
+  TableItem(head: '连接时间', width: 120, align: Alignment.center, getLabel: (c, _) => Day().useLocale(locale).from(Day.fromString(c.start))),
 ];
 
 class PageConnections extends StatefulWidget {
@@ -50,6 +64,8 @@ class _PageConnectionsState extends State<PageConnections> {
   IOWebSocketChannel? _channel;
   final ScrollController _horizontalScrollController = ScrollController();
   final ScrollController _verticalScrollController = ScrollController();
+
+  Map<String, ConnectionsConnection> _cachePreConnections = {};
 
   @override
   void initState() {
@@ -68,7 +84,13 @@ class _PageConnectionsState extends State<PageConnections> {
       stream: _channel!.stream,
       builder: (context, snapshot) {
         final data = snapshot.hasData ? Connections.buildFromJson(json.decode(snapshot.data as String)) : Connections.def;
-
+        final preConnections = _cachePreConnections;
+        _cachePreConnections = {};
+        for (var it in data.connections) {
+          _cachePreConnections[it.id] = it;
+        }
+        // TODO: sort
+        data.connections.sort((a, b) => a.metadata.host.compareTo(b.metadata.host));
         return Column(
           children: [
             CardHead(
@@ -95,14 +117,13 @@ class _PageConnectionsState extends State<PageConnections> {
                     children: [
                       Row(
                         children: tableItems
-                            .map((e) => Text(e.head, overflow: TextOverflow.ellipsis)
-                                .textColor(const Color(0xff909399))
-                                .fontSize(14)
-                                .alignment(Alignment.center)
-                                .width(e.width))
+                            .map((e) => TextButton(
+                                  child: Text(e.head, overflow: TextOverflow.ellipsis).textColor(const Color(0xff909399)).fontSize(14),
+                                  onPressed: () => {},
+                                ).alignment(Alignment.center).width(e.width))
                             .toList(),
                       ).height(30).backgroundColor(const Color(0xfff3f6f9)),
-                      ...data.connections.map((it) => _ConnectingItem(connection: it)).toList(),
+                      ...data.connections.map((it) => _ConnectingItem(connection: it, preConnection: preConnections[it.id])).toList(),
                     ],
                   ),
                 ),
@@ -124,14 +145,15 @@ class _PageConnectionsState extends State<PageConnections> {
 }
 
 class _ConnectingItem extends StatelessWidget {
-  const _ConnectingItem({Key? key, required this.connection}) : super(key: key);
+  const _ConnectingItem({Key? key, required this.connection, this.preConnection}) : super(key: key);
   final ConnectionsConnection connection;
+  final ConnectionsConnection? preConnection;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: tableItems.map((e) {
-        final label = e.getLabel(connection);
+        final label = e.getLabel(connection, preConnection);
         final text = Text(
           label,
           overflow: TextOverflow.ellipsis,
